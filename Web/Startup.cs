fad197419c3080b78ccea228676c1a8673132f04
MaartenGDev <maarten.grootoonk@gmail.com>
2019-08-04 21:55:51 +0200

    Use short commit hash in k8s deployment

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
 
 namespace Web
 {
@@ -80,6 +81,11 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMetricServer();
+            app.UseHttpMetrics();
+
+            app.UseExceptionHandler();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
